ff189ea91f11b23853e35f87522029b072832b42
daviddoji <daviddoji@protonmail.com>
2017-04-16 19:50:42 +0200

    update makeCopy_better.sh

--- makeCopy_better.sh
+++ makeCopy_better.sh
@@ -2,15 +2,14 @@
 
 DIR=Arreglados
 
-
 # si el directorio no existe lo crea
 if [ ! -d pwd/$DIR ]
 then
   mkdir $DIR
 fi
 
-# copia recursivamente desde el directorio actual todos los archivos 
-# con extension .mp4 o .mkv al directorio $DIR
+# copia recursivamente desde el directorio actual todos los archivos
+# con extension .m** al directorio $DIR
 find . -name "*.m**" -exec cp -t $DIR {} +
 
 # copia todos los subtitulos del directorio actual al directorio $DIR
@@ -18,4 +17,3 @@ rsync -a *.srt $DIR/
 
 # muestra notificacion de 2 s al acabar
 notify-send -t 2000 "Fin de la conversión"
-
